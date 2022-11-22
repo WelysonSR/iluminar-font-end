dcab4c2d695cb10cd51a4eccbbc637c8595a7535
Welyson Rosa <welysonsr@gmail.com>
2022-11-22 10:18:26 -0300

    feat: removendo react context

--- src/pages/HomeFuncionario.tsx
+++ src/pages/HomeFuncionario.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { TableDuo } from '../components/TableDuo';
-import { UserProvider } from '../context/UserProvider';
 
 export function HomeFunsionario() {
   const navigate = useNavigate();
@@ -17,14 +16,12 @@ export function HomeFunsionario() {
   });
 
   return (
-    <UserProvider>
-      <section className="flex flex-col items-center w-[428px] ml-auto mr-auto border-2 border-brown-600">
-        <Header />
-        <div className="w-[416px] mt-[60px] flex justify-between">
-          <TableDuo referente={'VT'} valor={'57.60'} />
-          <TableDuo referente={'BH'} valor={'00:10'} />
-        </div>
-      </section>
-    </UserProvider>
+    <section className="flex flex-col items-center w-[428px] ml-auto mr-auto border-2 border-brown-600">
+      <Header />
+      <div className="w-[416px] mt-[60px] flex justify-between">
+        <TableDuo referente={'VT'} valor={'57.60'} />
+        <TableDuo referente={'BH'} valor={'00:10'} />
+      </div>
+    </section>
   )
 }
